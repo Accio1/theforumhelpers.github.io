af4651a2dec4a4d3bead13e6adbcf8cacdc3ee3d
-Accio- <48496940+Accio1@users.noreply.github.com>
2021-06-29 21:11:49 -0400

    Update imports.js

--- resources/imports.js
+++ resources/imports.js
@@ -5,11 +5,10 @@ function reference(link) {
 	<a href="${link}" target="_parent">
 		<img src="${link}resources/icon.png" width="64px" height="64px" class="navHomeIcon">
 	</a>
-	<h1 class="navTitle" style="color:orange !important">The Forum Helpers</h1>
+	<h1 class="navTitle">The Forum Helpers</h1>
 	<a href="${link}forumhelpers" class="navButton" target="_parent">List Of Forum Helpers</a>
 	<a href="https://scratch.mit.edu/studios/3688309/" class="navButton" target="_blank">Our Scratch Studio</a>
 	<a href="https://theforumhelpers.github.io/QuickReply/" class="navButton" target="_parent">QuickReply</a>
-
 	<div class="expandableDropdown" onmouseover="expandHeader()" onmouseout="collapseHeader()">
 		<a class="expandableLink" href="${link}forumhelpers">List Of Forum Helpers</a>
 		<a class="expandableLink" href="https://scratch.mit.edu/studios/3688309/">Our Scratch Studio</a>
@@ -55,8 +54,8 @@ function denyPrivacy() {
 	window.location.href = "https://scratch.mit.edu/studios/3688309/";
 }
 
+var headerDropdown = document.getElementsByClassName("expandableDropdown")[0];
 function expandHeader() {
-	var headerDropdown = document.getElementsByClassName("expandableDropdown")[0]
 	headerDropdown.style.backgroundImage = "url('" + referenceLink + "resources/arrow.svg')";
 	headerDropdown.style.height = "60px";
 	var headerLinks = document.getElementsByClassName("expandableLink");
@@ -66,7 +65,8 @@ function expandHeader() {
 }
 
 function collapseHeader() {
-	document.getElementsByClassName("expandableDropdown")[0].style.backgroundImage = "url('" + referenceLink + "resources/expandable.svg')";
+	headerDropdown.style.backgroundImage = "url('" + referenceLink + "resources/expandable.svg')";
+	headerDropdown.style.height = "50px";
 	var headerLinks = document.getElementsByClassName("expandableLink");
 	for (k = 0; k < headerLinks.length; k++) {
 		headerLinks[k].style.display = "none";
